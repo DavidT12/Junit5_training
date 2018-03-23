1edadaf17b94832cce730e41771bec9e554c3739
DavidT12 <tuohydavid@hotmail.com>
2018-03-23 13:59:05 +0000

    Step 3.3 – Migrate assertThat

--- san-francisco-tourism/src/test/java/com/javaone/hol2017/junit5/FishermansWharfEnumTest.java
+++ san-francisco-tourism/src/test/java/com/javaone/hol2017/junit5/FishermansWharfEnumTest.java
@@ -1,11 +1,11 @@
 package com.javaone.hol2017.junit5;
 
 import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.time.*;
 
-import org.junit.*;
 
 public class FishermansWharfEnumTest {
 
