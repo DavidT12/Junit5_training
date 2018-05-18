9324450c0732e8a3f476b81a8d2391ad42f4b758
DavidT12 <tuohydavid@hotmail.com>
2018-05-18 12:30:07 +0100

    step 4.2 completed

--- san-francisco-tourism/src/test/java/com/javaone/hol2017/junit5/ThirtyNinthAnniversaryEventsTest.java
+++ san-francisco-tourism/src/test/java/com/javaone/hol2017/junit5/ThirtyNinthAnniversaryEventsTest.java
@@ -9,33 +9,18 @@ import java.util.*;
 import java.util.stream.*;
 
 import org.junit.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.runner.*;
 import org.junit.runners.*;
 import org.junit.runners.Parameterized.*;
 
-@RunWith(Parameterized.class)
+
 public class ThirtyNinthAnniversaryEventsTest {
 
-	@Parameter(0) public LocalDate date;
-	@Parameter(1) public boolean expectedResult;
-
-	@Parameters(name="{0}")
-	public static Collection<Object[]> testCases() throws IOException {
-		Path path = Paths.get("src/test/resources", "39-tests.txt");
-		return Files.lines(path)
-				// remove blank and commented out lines
-				.filter(l -> ! l.isEmpty())
-				.filter(l -> ! l.startsWith("#"))
-				// convert to array
-				.map(l -> l.split(","))
-				// convert to desired types for parameterized test
-				.map(a -> new Object[] { LocalDate.parse(a[0]), Boolean.parseBoolean(a[1])} )
-				// store in collection
-				.collect(Collectors.toList());
-	}
-
-	@Test
-	public void date() {
+	@ParameterizedTest(name = "{0}")
+	@CsvFileSource(resources = { "/39-tests.txt" })
+	public void date(LocalDate date, boolean expectedResult) {
 		boolean actual = ThirtyNinthAnniversaryEvents.isCelebrationDay(date);
 		assertEquals(expectedResult, actual);
 	}
